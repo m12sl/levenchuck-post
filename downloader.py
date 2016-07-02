6e32e70cd5b7a0516e7bee3ea055c882316e9d33
alex <m12sl@yandex.ru>
2016-07-02 19:50:10 +0300

    add text utils

---
+++ downloader.py
@@ -0,0 +1,140 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from time import time
+import os
+
+from multiprocessing import Pool
+import html2text
+
+
+import click
+
+
+def parse_post(url='http://ailev.livejournal.com/1274596.html'):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    try:
+        title = soup.find('h1', class_='entry-title').text.strip()
+    except:
+        title = ''
+    h = html2text.HTML2Text()
+    try:
+        content = soup.find('article', class_='entry-content').text.strip('\n ')
+        cleared = h.handle(content).strip('\n ')
+    except:
+        cleared = ''
+    if len(title) == 0 and len(cleared) == 0:
+        title = '#FAILED'
+    return (url, title, cleared)
+
+
+def list_days(root_url='http://ailev.livejournal.com/'):
+    """
+    :param root_url:
+    :return:
+     list of links like `user.livejournal.com/year/month/day/`
+    """
+    hrefs = []
+    for year in tqdm(range(2002, 2003)):
+        calendar = root_url + '{}/'.format(year)
+        r = requests.get(calendar)
+        if r.status_code != 200:
+            continue
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for x in soup.findAll('a'):
+            link = x.get('href', '')
+            if len(link) >= len(root_url) and link[:len(root_url)] == root_url:
+                poi = link[len(root_url):]
+                if re.match('(\d+)\/(\d+)\/(\d+)/', poi):
+                    hrefs.append(link)
+    return hrefs
+
+
+def list_posts(day_link):
+    hrefs = []
+
+    # just dirty filtering trick
+    root = day_link[:15]
+    r = requests.get(day_link)
+    if r.status_code != 200:
+        return None
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for x in soup.findAll('a'):
+        link = x.get('href', '')
+        if re.match('(.+)\/(\d+).html$', link) and link[:len(root)] == root:
+            hrefs.append(link)
+    return hrefs
+
+
+def all_links(root='http://ailev.livejournal.com/', nb=10, path='post-list.txt'):
+    print('Fetch calendar entries')
+    days = list_days(root_url=root)
+    print('There are {} days with entries'.format(len(days)))
+    t0 = time()
+
+    pool = Pool(processes=nb)
+    it = pool.imap_unordered(list_posts, days)
+    work = list(tqdm(it, total=len(days)))
+    pool.close()
+    pool.join()
+
+    links = []
+    for x in work:
+        if x:
+            links.extend(x)
+
+    links = list(set(links))
+    links.sort()
+    
+
+    with open(path, 'w') as fout:
+        fout.writelines(x + '\n' for x in links)
+
+    t1 = time()
+    print('Done for {}s'.format(t1 - t0))
+    return links
+
+
+def fetch_list(links, nb=4):
+    print('Going to mp load all {} pages'.format(len(links)))
+    t0 = time()
+    pool = Pool(processes=nb)
+    it = pool.imap_unordered(parse_post, links)
+    work = list(tqdm(it, total=len(links)))
+    pool.close()
+    pool.join()
+    t1 = time()
+    print('Done for {:.2f} minutes'.format((t1 - t0)/60))
+    work = [_ for _ in work if _]
+    work.sort(key=lambda t: t[0])
+
+    return work
+
+
+@click.command()
+@click.option('--nb', default=4, help='number of workers')
+@click.option('--save-dir', default='tmp/')
+def main(nb, save_dir):
+    try:
+        os.mkdir(save_dir)
+    except OSError:
+        pass
+
+    links = all_links(nb=nb, path=os.path.join(save_dir, 'post-list.txt'))
+
+    data = fetch_list(links, nb=nb)
+    print('Store data')
+    with open(os.path.join(save_dir, 'allj.txt'), 'w') as fout:
+        for (url, title, content) in tqdm(data):
+            fout.write('<post href="{}">{}\n'.format(url, title))
+            fout.write(content)
+            fout.write('\n</post>\n')
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+import re
+from collections import Counter
+
+from tqdm import tqdm
+
+
+def load_dumped(path='tmp/allj.txt'):
+    data = []
+    with open(path, 'r') as fin:
+        tmp = []
+        for line in tqdm(fin):
+            if re.match('<post', line):
+                title = line[line.find('>') + 1:]
+                continue
+            if re.match('<\/post>', line):
+                data.append((title, ''.join(tmp)))
+                tmp = []
+                continue
+            tmp.append(line)
+    return data
+
+
+def generate_charset():
+    rus = [chr(_) for _ in range(ord('а'), ord('я') + 1)]
+    eng = [chr(_) for _ in range(ord('a'), ord('z') + 1)]
+    alphabet = rus + eng + [_.upper() for _ in rus + eng]
+    nums = list('0123456789')
+    punkt = list(' !?,.\"\':-()><\n')
+    return alphabet + nums + punkt + ['oov']
+
+
+def generate_vocab():
+    charset = generate_charset()
+    vocab = {k: i + 1 for i, k in enumerate(charset)}
+    vocab[''] = 0
+    return vocab
+
+
+def encode_text(text, vocab):
+    dtype = np.dtype(np.uint8)
+
+    encoded = np.zeros((len(text), ), dtype=dtype)
+    oov = vocab.get('oov', 0)
+
+    idx = 0
+    last = 0
+    for x in tqdm(text):
+        e = vocab.get(x, oov)
+        if e == oov and last == oov:
+            continue
+        last = e
+        encoded[idx] = e
+        idx += 1
+
+    return encoded[:idx]
+
+
+def alphabet(data):
+    titles = Counter()
+    texts = Counter()
+    for x in data:
+        titles.update(x[0])
+        texts.update(x[1])
+    print('Title stats:', titles)
+    print('Texts stats:', texts)
+
+
+if __name__ == "__main__":
+    data = load_dumped()
+
+    alphabet(data)
+
+
+    idx = np.arange(len(data))
+    np.random.seed(19)
+    np.random.shuffle(idx)
+
+    data = [data[_] for _ in idx]
+    val_len = 10
+    val_posts, train_posts = data[:val_len], data[val_len:]
+
+    # take only content:
+    
+    val_text = "".join(_[1] for _ in val_posts)
+    train_text = "".join(_[1] for _ in train_posts)
+    
+    vocab = generate_vocab()
+    val = encode_text(val_text, vocab)
+    train = encode_text(train_text, vocab)
+
+    np.save('val', val)
+            
+
+
